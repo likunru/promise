40e7627217207ce6013adbf03a095db16ed05cf4
likunru <kunru.li@capitalonline.net>
2019-07-23 18:26:35 +0800

    init

--- callback/transaction.js
+++ callback/transaction.js
@@ -1,5 +1,31 @@
+// transaction作用在目标函数之前或之后进行一些操作，
+// 例：在做真正业务之前，需要进行验证、授权或者输出日志的操作。
+
 class Transaction {
     perform (anyMethod, wrappers) {
-
+      wrappers.forEach(wrapper => wrapper.initialize());
+      anyMethod();
+      wrappers.forEach(wrapper => wrapper.close());
     }
 }
+
+let transaction = new Transaction();
+let oldFunc = () => {
+    console.log('所有的逻辑');
+}
+
+transaction.perform(oldFunc,[{
+    initialize () {
+        console.log('初始化1');
+    },
+    close () {
+        console.log('关闭1');
+    }
+},{
+    initialize () {
+        console.log('初始化2');
+    },
+    close () {
+        console.log('关闭2');
+    }
+}])
